a2f9ecf4398d034669d4ff2a99d716710499f90e
Rashini De Silva <87619840+rashpr88@users.noreply.github.com>
2022-11-24 12:15:51 +0530

    Add files via upload
    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace temperature
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.name = new System.Windows.Forms.Label();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.t2 = new System.Windows.Forms.TextBox();
+            this.Celcius = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.r1 = new System.Windows.Forms.RadioButton();
+            this.r2 = new System.Windows.Forms.RadioButton();
+            this.b1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.BackColor = System.Drawing.Color.Cyan;
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(61, 25);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(239, 24);
+            this.name.TabIndex = 1;
+            this.name.Text = "Temperature Convereter";
+            // 
+            // t1
+            // 
+            this.t1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t1.Location = new System.Drawing.Point(78, 81);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(100, 22);
+            this.t1.TabIndex = 2;
+            this.t1.TextChanged += new System.EventHandler(this.t1_TextChanged);
+            this.t1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.t1_KeyPress);
+            // 
+            // t2
+            // 
+            this.t2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t2.Location = new System.Drawing.Point(78, 131);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(100, 22);
+            this.t2.TabIndex = 3;
+            this.t2.TextChanged += new System.EventHandler(this.t2_TextChanged);
+            this.t2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.t2_KeyPress);
+            // 
+            // Celcius
+            // 
+            this.Celcius.AutoSize = true;
+            this.Celcius.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Celcius.Location = new System.Drawing.Point(12, 87);
+            this.Celcius.Name = "Celcius";
+            this.Celcius.Size = new System.Drawing.Size(52, 16);
+            this.Celcius.TabIndex = 4;
+            this.Celcius.Text = "Celcius";
+            this.Celcius.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Farenhite";
+            // 
+            // r1
+            // 
+            this.r1.AutoSize = true;
+            this.r1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r1.Location = new System.Drawing.Point(239, 116);
+            this.r1.Name = "r1";
+            this.r1.Size = new System.Drawing.Size(82, 20);
+            this.r1.TabIndex = 7;
+            this.r1.TabStop = true;
+            this.r1.Text = "Farenhite";
+            this.r1.UseVisualStyleBackColor = true;
+            this.r1.CheckedChanged += new System.EventHandler(this.r1_CheckedChanged);
+            this.r1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.r1_MouseClick);
+            // 
+            // r2
+            // 
+            this.r2.AutoSize = true;
+            this.r2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r2.Location = new System.Drawing.Point(239, 142);
+            this.r2.Name = "r2";
+            this.r2.Size = new System.Drawing.Size(70, 20);
+            this.r2.TabIndex = 8;
+            this.r2.TabStop = true;
+            this.r2.Text = "Celcius";
+            this.r2.UseVisualStyleBackColor = true;
+            this.r2.CheckedChanged += new System.EventHandler(this.r2_CheckedChanged);
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.Bisque;
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(126, 191);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(95, 32);
+            this.b1.TabIndex = 9;
+            this.b1.Text = "Convert";
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(236, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Convert in to : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 250);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.r2);
+            this.Controls.Add(this.r1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Celcius);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.TextBox t2;
+        private System.Windows.Forms.Label Celcius;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton r1;
+        private System.Windows.Forms.RadioButton r2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace temperature
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void r2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void r1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void t1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void t1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (! Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true ;
+            }
+        }
+
+        private void t2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void t2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void r1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            if (r1.Checked == true)
+            {
+                if (t1.Text != "")
+                {
+                    double temp = Convert.ToDouble(t1.Text);
+                    double res ;
+                    res = ((9.0 / 5.0) * temp) + 32.0;
+                    t2.Text = res.ToString();
+
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a value for temperature");
+                }
+            }
+            else if (r2.Checked == true)
+            {
+                if (t2.Text != "")
+                {
+                    double temp = Convert.ToDouble(t2.Text);
+                    double res;
+                    res = (temp - 32.0) * (5.0 / 9.0);
+                    t1.Text = Convert.ToString(res);
+
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a value for temperature");
+                }
+            }
+            else if (t2.Text == "" && t1.Text == "")
+            {
+                MessageBox.Show("Please enter a value for temperature first and select the conversion");
+            }
+
+            else
+            {
+                MessageBox.Show("Please select a convesion type");
+            }
+
+        }
+    }
+}
